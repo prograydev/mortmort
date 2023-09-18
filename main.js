aef3207348ec0033b0cf73ba7cbaf75afc7b451b
John Carlo Buhay <122296586+prograydev@users.noreply.github.com>
2023-09-18 22:59:41 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let changeText = document.getElementById('message');
+let bg = document.getElementById('bg');
+let btn = document.getElementById('btn');
+
+function btnClick(){
+    
+    changeText.innerHTML = "Hi Babi mort, ILOVEYOUUU ALWAYS!<3";
+    bg.style.backgroundImage = "url('mortmort.jpg')";
+
+    btn.style.visibility = 'hidden';
+}
